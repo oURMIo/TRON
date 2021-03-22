e8fbecaea53dc9393bde609c182c86a841072e13
URMI <dimach.98.ru@gmail.com>
2021-03-22 21:50:21 +0300

    prov2

--- Source1.cpp
+++ Source1.cpp
@@ -15,6 +15,11 @@ public:
 // глобальные переменные ЗЛО! НО очень удобны для таких мелких программ =)
 int pole[30 /*x*/][20 /*y*/] = {}; // [width][height]
 
+// раз может быть от 2 до 4 - создадим массив на всех!
+Player players[4];
+const std::string null = "";    //trash)
+
+
 //direction(направление)  движения
 int direction[4 /*стороны*/][2 /*x,y*/] = { /*налево*/ { -1, 0 }, /*направо*/ { 1, 0 }, /*вниз*/ { 0, 1 }, /*вверх*/ { 0, -1 } };
 
@@ -71,7 +76,7 @@ bool isFree(int x, int y, int moveIndex)
 }
 
 // движемся!
-string fist_strat(Player& player)
+string fist_strat(Player& player)   //
 {
     // двигаем в прежнем направлении, если можем
     if (isFree(player.x, player.y, player.moveIndex)) {
@@ -96,64 +101,70 @@ string move_index(Player& player,int index) {
     player.moveIndex = index;
     return toString(index);
 }
-
-bool prov = true;
+  
 
 // 2-я страта но я не знаю как ее вставить туда
 string two_strat(Player& player)        //0-left / 1-right / 2-down / 3-up
 {
-    int rastvp, rastleft, rastright;
-    //last_move не объявлена и вообще не знаю куда ее засунать
-    if (last_move == "UP")
+    return null;
+    int rastvp, rastleft, rastright, s_left,s_right;
+    if (player.moveIndex == 3)
     {
         rastvp = player.y;
         rastleft = player.x;
         rastright = 30 - player.x;
-        if rastleft >= rastright)
+        s_left = rastvp * rastleft;
+        s_right = rastvp * rastright;
+        if (rastleft >= rastright)
             return move_index(player, 0);
         else
             return move_index(player, 1);
     }
 
-    if (last_move == "DOWN")
+    if (player.moveIndex == 2)
     {
         rastvp = 20 - player.y;
         rastleft = 30 - player.x;
         rastright = player.x;
-        if rastleft >= rastright)
+        if (rastleft >= rastright)
             return move_index(player, 0);
         else
             return move_index(player, 1);
     }
 
-    if (last_move == "RIGHT")
+    if (player.moveIndex == 1)
     {
         rastvp = 30 - player.x;
         rastleft = player.y;
         rastright = 20 - player.y;
-        if rastleft >= rastright)
+        if (rastleft >= rastright)
             return move_index(player, 0);
         else
             return move_index(player, 1);
     }
 
-    if (last_move == "LEFT")
+    if (player.moveIndex == 0)
     {
         rastvp = player.x;
         rastleft = 20 - player.y;
         rastright = player.y;
-        if rastleft >= rastright)
+        if (rastleft >= rastright)
             return move_index(player, 0);
         else
             return move_index(player, 1);
     }
+
     cerr << endl << "приплыли" << endl;
+    return null;
 }
 
 
+bool prov = true;
+bool prov2 = true;
+
 string lets_move(Player& player)     //1 strata
 {
-    if (prov == true)
+    if (prov)
     {
         if (player.y < 10 && isFree(player.x, player.y, 2))
             return move_index(player,2);
@@ -166,7 +177,71 @@ string lets_move(Player& player)     //1 strata
         prov = false;
     }
 
-    return fist_strat(player);
+    
+    if (prov2) {
+        Player& enemy = player;     //enemy
+        for (int i = 0; i < 4; i++) {
+            if (players[i].x != player.x || players[i].y != player.y) {
+                enemy = players[i];
+                break;
+            }
+        }
+        // враг справа
+        if (player.x <= enemy.x) 
+        {
+            cerr << "PROVERKA1" << endl;
+
+            if (player.y <= enemy.y)    //враг сверху
+            {
+                if (enemy.moveIndex == 0 && enemy.moveIndex == 3)
+                    return move_index(player, 3);
+                if (enemy.moveIndex == 1 && enemy.moveIndex == 2)
+                    return move_index(player, 1);
+            }
+            else    // враг снизу
+            {
+                if (enemy.moveIndex == 0 && enemy.moveIndex == 2)
+                    return move_index(player, 2);
+                if (enemy.moveIndex == 3 && enemy.moveIndex == 1)
+                    return move_index(player, 1);
+            }
+        }
+        else
+        {/*
+            if (player.y <= enemy.y)    //враг сверху
+            {
+                if (enemy.moveIndex == 1 && enemy.moveIndex == 3)
+                    return move_index(player, 3);
+                if (enemy.moveIndex == 1 && enemy.moveIndex == 2)
+                    return move_index(player, 1);
+            }
+            else    // враг снизу
+            {
+                if (enemy.moveIndex == 0 && enemy.moveIndex == 2)
+                    return move_index(player, 2);
+                if (enemy.moveIndex == 3 && enemy.moveIndex == 1)
+                    return move_index(player, 1);
+            }
+            */
+
+        }
+
+        prov2 = false;
+    }
+
+    //return fist_strat(player);
+    //return two_strat(player);
+    string result = two_strat(player);
+    if (result == null)
+    {
+        cerr << "LOOLLOOL" << endl;
+        return fist_strat(player);
+    }
+    else
+    {
+        cerr << "222222222LOOLLOOL" << endl;
+        return result;
+    }
 }
 // cout << "UP" << endl; // A single line with UP, DOWN, LEFT or RIGHT
 
@@ -177,9 +252,6 @@ int main()
     int X1, Y1; // starting X,Y coordinate of lightcycle (can be the same as X0 if you play before this player)
     int PX = -100, PY = -100;
     int move = 0;
-
-    // раз может быть от 2 до 4 - создадим массив на всех!
-    Player players[4];
 
     while (1) {
         move++;
@@ -199,6 +271,29 @@ int main()
 
                 players[i].x = X0;      //omega присваивани
                 players[i].y = Y0;
+            }
+            else
+            {
+                // пошёл влево
+                if (players[i].x > X1) 
+                {
+                    players[i].moveIndex = 0;
+                }
+                else if (players[i].x < X1) 
+                {
+                    // пошёл вправо
+                    players[i].moveIndex = 1;
+                }
+                else if (players[i].y < Y1) 
+                {
+                    // вниз
+                    players[i].moveIndex = 2;
+                }
+                else if (players[i].y > Y1) 
+                {
+                    // вверх
+                    players[i].moveIndex = 3;
+                }
             }
             // инициализируем i-го игрока.
             players[i].x = X1;
